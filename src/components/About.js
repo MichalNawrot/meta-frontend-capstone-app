2cc4ee7a0cfd0ac1489d012abd7da7dbff6148e9
Fergus Findley <fergus.findley@gmail.com>
2023-10-30 18:35:05 +0100

    - change look and feel of about and hero - further improvements of CSS

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ const About = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
             <div id="photos">
-                <img src="images/restaurant-Mario-and-Adrian-B.jpg" alt="Mario and Adrian" width="100px"></img>
-                <img src="images/restaurant-Mario-and-Adrian-A.jpg" alt="Mario and Adrian" width="100px"></img>
+                <img src="images/restaurant-Mario-and-Adrian-B.jpg" alt="Mario and Adrian"></img>
+                <img src="images/restaurant-Mario-and-Adrian-A.jpg" alt="Mario and Adrian"></img>
             </div>
         </div>
     );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,16 @@
 const Hero = () => {
     return (
         <div id="hero">
-            <div>
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-                <button>Reserve a Table</button>
-            </div>
-            <div>
-                <img src="images/restaurant-food.jpg" alt="Little Lemon food"></img>
+            <div id="hero-content">
+                <div id="hero-text">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                    <button>Reserve a Table</button>
+                </div>
+                <div id="hero-img">
+                    <img src="images/restaurant-food.jpg" alt="Little Lemon food"></img>
+                </div>
             </div>
         </div>
     );
