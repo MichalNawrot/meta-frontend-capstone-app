5012eaea87a1846c10a93139aa38a99d60156e57
Fergus Findley <fergus.findley@gmail.com>
2023-11-04 20:35:13 +0100

    add link to logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import Nav from './Nav';
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <>
             <header>
-                <img src="images/logo-text-color.png" alt="logo Little Lemon"></img>
+                <Link to="/">
+                    <img src="images/logo-text-color.png" alt="logo Little Lemon"></img>
+                </Link>
             </header>
             <Nav />
         </>
